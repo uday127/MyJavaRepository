ed65e776726b89fa9b7f07d75625faf7a808ae3e
Uday Wagle <uday_wagle@persistent.co.in>
2018-07-26 15:24:18 +0530

    dispalyDate by object

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,7 +1,8 @@
 Class HelloWorld{
 
-    vOid displayDate(){
-       System.out.println("display today's date:-");
+   static vOid displayDate(){
+      
+    System.out.println("display today's date:-");
 
 
   }
@@ -10,9 +11,12 @@ Class HelloWorld{
      public static void main(Strin[] args){
 
     system.out.println("Hello World....Welcome to Git");
+        
+        displayDate();
 
-HelloWorld obj =new HelloWorld;
-obj.displayDate();
+     HelloWorld obj =  new HelloWorld;
+       
+     obj.displayDate();
 
 
     }
