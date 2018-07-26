0424f5c979641bebb8003122521dcb5fa467f28e
Uday Wagle <uday_wagle@persistent.co.in>
2018-07-26 15:09:32 +0530

    dispalyDate method.....

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,7 +1,8 @@
 Class HelloWorld{
 
-    vOid displayDate(){
-       System.out.println("display today's date:-");
+   static vOid displayDate(){
+      
+    System.out.println("display today's date:-");
 
 
   }
@@ -10,6 +11,8 @@ Class HelloWorld{
      public static void main(Strin[] args){
 
     system.out.println("Hello World....Welcome to Git");
+        
+        displayDate();
 
 
     }
